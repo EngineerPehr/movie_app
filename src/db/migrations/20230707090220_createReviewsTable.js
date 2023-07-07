f02ca8c208a8f591ede5abcc2169914cb80fb97d
EngineerPehr <dragontoothforge@gmail.com>
2023-07-07 09:26:08 -0600

    Added knex migrations for the following tables: critics, movies_theaters, movies, reviews, and theaters

---
+++ src/db/migrations/20230707090220_createReviewsTable.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('reviews', (table) => {
+        table.increment('review_id').primary()
+        table.text('content')
+        table.integer('score')
+        table.integer('critic_id')
+        table
+            .foreign('critic_id')
+            .references('critic_id')
+            .inTable('critics')
+            .onDelete('cascade')
+        table.integer('movie_id')
+        table
+            .foreign('movie_id')
+            .references('movie_id')
+            .inTable('movies')
+            .onDelete('cascade')
+        table.timestamps(true, true)
+    })
+}
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('reviews')
+}
